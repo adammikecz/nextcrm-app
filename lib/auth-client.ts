61727e46322c6eaffc0dd129cf93d4cd68bfe8fd
adammikecz <33648021+adammikecz@users.noreply.github.com>
2026-06-17 11:55:11 +0200

    Update auth-client.ts

--- lib/auth-client.ts
+++ lib/auth-client.ts
@@ -2,13 +2,22 @@ import { createAuthClient } from "better-auth/react";
 import { emailOTPClient, adminClient } from "better-auth/client/plugins";
 import { ac, admin, manager, user } from "@/lib/auth-permissions";
 
+const baseURL =
+  typeof window !== "undefined"
+    ? window.location.origin
+    : process.env.NEXT_PUBLIC_APP_URL;
+
 export const authClient = createAuthClient({
-  baseURL: process.env.NEXT_PUBLIC_APP_URL,
+  baseURL,
   plugins: [
     emailOTPClient(),
     adminClient({
       ac,
-      roles: { admin, manager, user },
+      roles: {
+        admin,
+        manager,
+        user,
+      },
     }),
   ],
 });
